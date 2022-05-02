7ef7746a0a858a392860b2dc499c30b6334de128
lmbelo <lucas.belo@live.com>
2022-05-02 08:00:23 -0700

    Extending the None conf_args to an empty list

--- python3-macos/MacOS/configure.py
+++ python3-macos/MacOS/configure.py
@@ -7,15 +7,11 @@ def main():
     args, remaining = parse_args()
     os.environ.update(env_vars(args.target_arch_name))
 
-    conf_args = ARCHITECTURES[args.target_arch_name].CONF_ARGS
-    if conf_args is None:
-        conf_args = []
-
     cmd = [
         'bash', 
             './configure',
             '--enable-shared',
-            ' '.join(ARCHITECTURES[args.target_arch_name].CONF_ARGS)
+            ' '.join(ARCHITECTURES[args.target_arch_name].CONF_ARGS or [])
     ]  
 
     os.execvp('bash', cmd + remaining)
