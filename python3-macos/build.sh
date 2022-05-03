2278d9b473fff429d2ec4ca7aeba418dacda7c78
lmbelo <lucas.belo@live.com>
2022-05-02 21:09:21 -0700

    Upgrading GCC to try it as a fix for the generate-posix-var issue

--- python3-macos/build.sh
+++ python3-macos/build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+gcc --version
+brew upgrade gcc
+gcc --version
+
 if [ $ARCH = "x86_64" ] || [ $ARCH = "universal2" ]; then
     echo "Building Python for $ARCH"
     mkdir $ARCH    
