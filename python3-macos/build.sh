ac6538bf9c4ac95122ea405fa40bc8b12ed1a3bd
lmbelo <lucas.belo@live.com>
2022-05-04 17:33:14 -0700

    Creating the pre-built routine

--- python3-macos/build.sh
+++ python3-macos/build.sh
@@ -26,8 +26,8 @@ mkdir -p src
 pushd src
 
 # Download Python
-curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
-tar --no-same-owner -xf Python-$PYVER.tar.xz
+#curl -vLO https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tar.xz
+#tar --no-same-owner -xf Python-$PYVER.tar.xz
 
 popd
 
@@ -35,6 +35,7 @@ popd
 
 # Create the pre-built directory by extracting the zip file
 mv -f ../MacOS/prebuiltdeps.tar.gz "$THIS_DIR/build"
+ls -l
 tar --no-same-owner -xf "$THIS_DIR/build/prebuiltdeps.tar.gz"
 
 # Copy our custom build-script to the BuildScript folder
